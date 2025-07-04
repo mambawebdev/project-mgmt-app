fcfaccbdfd1d058ec18f33014891b62fb75d75c3
Tony L <155401134+mambawebdev@users.noreply.github.com>
2025-07-03 21:16:01 -0400

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const cache = new InMemoryCache({
 })
 
 const client = new ApolloClient({
-    uri: "http://localhost:5000/graphql",
+    uri: "https://mambawebdev.github.io/project-mgmt-app/",
     cache
 })
 
